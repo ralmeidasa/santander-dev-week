5efd508714fb86c44e5e3ad5f559b2f9fa6d5ef8
Rafael Almeida <rafaelalmeidasa@gmail.com>
2023-10-13 11:53:03 -0300

    feat: create UserRepository

---
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package me.dio.domain.repository;
+
+import me.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByAccountNumber (String accountNumber);
+
+    boolean existsByCardNumber(String number);
+}
